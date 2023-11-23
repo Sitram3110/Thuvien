62e13d057decbdbd54f92cf0e857e0e10dde3613
NghiaNguyen04 <tuannghia20042015@gmail.com>
2023-11-23 14:25:26 +0700

    update chiTietSach trong Trang doc gia

--- src/DAO/KetNoiSQL.java
+++ src/DAO/KetNoiSQL.java
@@ -4,22 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+//password máy Trâm: duckg0946945409
 
 public class KetNoiSQL {
+    public static String databaseName = "QuanLyThuVien";
+
     public static Connection getConnection() {
         String url = "net.sourceforge.jtds.jdbc.Driver";
         try {
             Class.forName(url);
-            String dbUrl = "jdbc:sqlserver://DESKTOP-KRD2V7D: 1433;" +
-                    "user=sa; password=123; databaseName=QuanLyThuVien; encrypt=false";
-            return DriverManager.getConnection(dbUrl);
-//          return DriverManager.getConnection(dbUrl,user, pass);
+            String dbUrl = "jdbc:jtds:sqlserver://localhost:1433/" + databaseName;
+            return DriverManager.getConnection(dbUrl, "sa", "duckg0946945409");
+
+            // return DriverManager.getConnection(dbUrl,user, pass);
+
         } catch (Exception ex) {
             Logger.getLogger(KetNoiSQL.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Connected to SQL server in catch");
             return null;
+
         }
     }
+
     public static void main(String[] args) {
         KetNoiSQL.getConnection();
+        System.out.println("Connected to SQL server");
     }
 }
